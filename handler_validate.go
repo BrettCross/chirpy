6ccf678b5952b21b403b7193ba10c5be6329d2cf
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2026-03-26 13:33:53 -0400

    feat: add profanity cleaning to /api/validate_chirp endpoint

--- handler_validate.go
+++ handler_validate.go
@@ -3,15 +3,33 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
+func cleanChirp(msg string, words map[string]struct{}) string {
+	profane := "****"
+	msgWords := strings.Split(msg, " ")
+	for i, msgWord := range msgWords {
+		if _, ok := words[strings.ToLower(msgWord)]; ok {
+			msgWords[i] = profane
+		}
+	}
+	return strings.Join(msgWords, " ")
+}
+
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	profanity := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert": {},
+		"fornax": {},
+	}
+
 	type parameters struct {
 		Body string `json:"body"`
 	}
 
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,8 +45,8 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
-	
+	cleanedChirp := cleanChirp(params.Body, profanity)
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanedChirp,
 	})
 }
